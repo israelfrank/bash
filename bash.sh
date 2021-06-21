2814a280b2415da6eca2f576853ba29edf1527c0
israel frank <israel.frank@sap.com>
2021-06-21 19:31:21 +0300

    ddaasd

--- bash.sh
+++ bash.sh
@@ -16,7 +16,7 @@ gpr() {
     # Push to origin, grabbing the output but then echoing it back.
     push_output=`git push origin -u ${branch} 2>&1`
 
-    git pull-request -m "$(git log -1 --pretty=%B)"
+    git pull-request -m "llllll"
     #  pr_url=https://github.com/israelfrank/bash/compare/main...$branch
 
 open $pr_url
