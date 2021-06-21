275c2c7c3a1a86e563e0c4c23cb49716c6e463de
israel frank <israel.frank@sap.com>
2021-06-21 17:50:02 +0300

    asd

--- bash.sh
+++ bash.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-pull_request() {
-  to_branch=$1
-  if [ -z $to_branch ]; then
-    to_branch="master"
-  fi
-  
-  # try the upstream branch if possible, otherwise origin will do
-  upstream=$(git config --get remote.upstream.url)
-  origin=$(git config --get remote.origin.url)
-  if [ -z $upstream ]; then
-    upstream=$origin
-  fi
-  
-  to_user=$(echo $upstream | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
-  from_user=$(echo $origin | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
-  repo=$(basename `git rev-parse --show-toplevel`)
-  from_branch=$(git rev-parse --abbrev-ref HEAD)
-  open "https://github.com/$to_user/$repo/pull/new/$to_user:$to_branch...$from_user:$from_branch"
+# Colour constants for nicer output.
+GREEN='\033[0;32m'
+RESET='\033[0m'
+
+# Push the current branch to origin, set upstream, open the PR page if possible.
+gpr() {
+
+    git checkout PR || git checkout -b PR
+    # Get the current branch name, or use 'HEAD' if we cannot get it.
+    branch=${branch:-HEAD}
+
+    # Pushing take a little while, so let the user know we're working.
+    echo "Opening pull request for ${GREEN}${branch}${RESET}..."
+
+    # Push to origin, grabbing the output but then echoing it back.
+    push_output=`git push origin -u ${branch} 2>&1`
+    echo ""
+    echo ${push_output}
 }
 
-pull_request
+gpr
--- code.sh
+++ code.sh
@@ -3,5 +3,10 @@
 echo hello world
 echo mosh
 echo david
-echo dsjdsfndshbvdfjvdfvdfbvd
+echo dsjdsfndshbvdfjvdfvdfbvd5
+echo dsjdsfndshbvdfjvdfvdfbvd4
+echo dsjdsfndshbvdfjvdfvdfbvd3
+echo dsjdsfndshbvdfjvdfvdfbvd2
+echo dsjdsfndshbvdfjvdfvdfbvd1
+echo 80777777
 source ./bash.sh
