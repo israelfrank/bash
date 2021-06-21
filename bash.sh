f143fe978df9685aed392b4bd9cad28f0013cc62
israel frank <israel.frank@sap.com>
2021-06-21 18:12:53 +0300

    ddaasd

--- bash.sh
+++ bash.sh
@@ -5,7 +5,7 @@
 # Push the current branch to origin, set upstream, open the PR page if possible.
 gpr() {
 
-    git checkout PR1  || git checkout -b PR1
+    git checkout PR1  
     # Get the current branch name, or use 'HEAD' if we cannot get it.
     git branch
     echo $branch
@@ -17,7 +17,7 @@ gpr() {
 
     pr_url=https://github.com/israelfrank/bash/compare/main...$branch
 
-open $pr_url
+# open $pr_url
 
 }
 
