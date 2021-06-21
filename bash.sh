2ec7d43766175eabb4eac74ec308978feedd20ec
israel frank <israel.frank@sap.com>
2021-06-21 18:14:42 +0300

    ddaasd

--- bash.sh
+++ bash.sh
@@ -7,7 +7,8 @@ gpr() {
 
     git checkout PR1  || git checkout -b PR1
     # Get the current branch name, or use 'HEAD' if we cannot get it.
-    branch=${branch:-HEAD}
+    branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+    
     echo $branch
     # Pushing take a little while, so let the user know we're working.
     git add .
