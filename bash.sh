60751502bb3624ac8b7a2edca6bcefff02e0d15b
israel frank <israel.frank@sap.com>
2021-06-21 18:00:05 +0300

    ddaasd

--- bash.sh
+++ bash.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Colour constants for nicer output.
-GREEN='\033[0;32m'
-RESET='\033[0m'
+#
 
 # Push the current branch to origin, set upstream, open the PR page if possible.
 gpr() {
@@ -12,11 +10,13 @@ gpr() {
     branch=${branch:-HEAD}
 
     # Pushing take a little while, so let the user know we're working.
-    echo "Opening pull request for ${GREEN}${branch}${RESET}..."
     git add .
     git commit -m "ddaasd"
     # Push to origin, grabbing the output but then echoing it back.
     push_output=`git push origin -u ${branch} 2>&1`
+
+    pr_url=https://$repository_url/compare/$master...$branch
+
     echo ""
     echo ${push_output}
 }
