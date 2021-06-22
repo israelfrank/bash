4bd252b6bb57830bb6a8c33834ee322444260d6e
israel frank <israel.frank@sap.com>
2021-06-22 14:11:47 +0300

    generate by shmok ventil

--- bash.sh
+++ bash.sh
@@ -5,21 +5,28 @@
 # Push the current branch to origin, set upstream, open the PR page if possible.
  gpr() {
 
-     git checkout -b PR-"$JOB_NAME/$BUILD_NUMBER"
+    #  git checkout -b PR-"$JOB_NAME/$BUILD_NUMBER"
+    #  git checkout -b PR
 #     # Get the current branch name, or use 'HEAD' if we cannot get it.
      branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
     
     echo $branch
 #     # Pushing take a little while, so let the user know we're working.
      git add .
-     git commit -m "ddaasd"
+    
+    msg="generate by shmok ventil"
+     git commit -m "$msg"
 #     # Push to origin, grabbing the output but then echoing it back.
+     
      push_output=`git push origin -u ${branch} 2>&1`
 
-      pr_url=https://github.com/israelfrank/bash/compare/main...$branch
+    #   pr_url=https://github.com/israelfrank/bash/compare/main...$branch
 
-    open $pr_url
-
+    # open $pr_url
+        curl -fk -H  -X POST \
+        https://github.com/api/v3/repos/israelfrank/bash/pulls \
+        -d "{\"title\":\"$msg\",\"base\":\"main\",\"head\":\"$branch\"}" \
+        | jq -r '.html_url'
     }
 
     gpr
